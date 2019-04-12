8a2b227b788de9b9ecf2749c8a9d4cf49db06914
Joseph Vick <joevick@vick80.com>
2019-04-11 22:14:41 -0400

    initialize stack as array and set push method

---
+++ W1D5/diy-adts.rb
@@ -0,0 +1,17 @@
+class Stack
+    def initialize
+        Array.new
+    end
+
+    def push(el)
+      self << el
+    end
+
+    def pop
+      # removes one element from the stack
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+    end
+  end
